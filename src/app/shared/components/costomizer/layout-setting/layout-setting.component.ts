dc1e11e18c7a5437e0eaed268dfa75d454a7db54
Bhautikkarmawebiots <bhautik.karma@webiots.com>
2023-06-29 14:02:56 +0530

    tivo theme
    update layout

--- src/app/shared/components/costomizer/layout-setting/layout-setting.component.ts
+++ src/app/shared/components/costomizer/layout-setting/layout-setting.component.ts
@@ -1,5 +1,6 @@
-import { Component, HostBinding, HostListener } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { DOCUMENT } from '@angular/common';
+import { Component, HostBinding, HostListener, Inject } from '@angular/core';
+import { ActivatedRoute, Router, RouterOutlet } from '@angular/router';
 import { LayoutService } from 'src/app/shared/services/layout/layout.service';
 
 @Component({
@@ -18,9 +19,31 @@ export class LayoutSettingComponent {
   @HostBinding('@.disabled')
   public selectedValue: any;
 
-  constructor(public layout: LayoutService) {}
+  constructor(
+    public layout: LayoutService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private activatedRoute: ActivatedRoute,
+    @Inject(DOCUMENT) private document: Document) {}
+    layoutvalue: string;
+    sidebarvalue : string
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.route.queryParams
+      .subscribe((params:any) => {
+        this.layoutvalue = params.layout;
+        this.sidebarvalue = params.sidebar;
+        console.log(this.sidebarvalue);
+        
+      //  this.document.body.classList.add(params.layout);
+      }
+    );
+
+    document.getElementsByTagName('html')[0].setAttribute('dir', this.layoutvalue);
+    document.getElementsByTagName('body')[0].setAttribute('class', this.layoutvalue);
+    document.getElementsByTagName('body')[0].setAttribute('had', this.sidebarvalue);
+    this.layout.config.settings.sidebar = this.sidebarvalue ? this.sidebarvalue : 'compact-wrapper';
+  }
 
   @HostListener('window:resize', ['$event'])
   onResize(event: any) {
@@ -34,24 +57,69 @@ export class LayoutSettingComponent {
   customizeSidebarType(val: any) {
     this.sidebarType = val;
     this.layout.config.settings.sidebar = val;
-    // if (val == 'horizontal-wrapper') {
-    //   document.getElementsByTagName('body')[0].setAttribute('had', val);
-    // } else {
-    //   document.getElementsByTagName('body')[0].removeAttribute('had');
-    // }
+    if (val == 'horizontal-wrapper') {
+      this.router.navigate(
+        [], 
+        {
+          relativeTo: this.activatedRoute,
+          queryParams: { sidebar: 'horizontal-wrapper' },
+          queryParamsHandling: 'merge'
+        });
+
+      document.getElementsByTagName('body')[0].setAttribute('had', val);
+
+    } else {
+      this.router.navigate(
+        [], 
+        {
+          relativeTo: this.activatedRoute,
+          queryParams: { sidebar: 'compact-wrapper' },
+          queryParamsHandling: 'merge'
+        });
+
+      document.getElementsByTagName('body')[0].removeAttribute('had');
+
+    }
   }
 
   customizeLayoutType(val: any) {
+    this.router.navigate(
+      [], 
+      {
+        relativeTo: this.activatedRoute,
+        queryParams: { layout: 'box-layout' },
+        queryParamsHandling: 'merge'
+      });
+
     this.layoutType = val;
     this.layout.config.settings.layout_type = val;
     document.getElementsByTagName('html')[0].removeAttribute('dir');
     document.getElementsByTagName('body')[0].removeAttribute('class');
 
     document.body?.classList.remove('box-layout');
-    if (val == 'rtl' || val == 'ltr') {
+    
+    if(val == 'rtl'){
+      this.router.navigate(
+        [], 
+        {
+          relativeTo: this.activatedRoute,
+          queryParams: { layout: 'rtl' },
+          queryParamsHandling: 'merge'
+        });
+
       document.getElementsByTagName('html')[0].setAttribute('dir', val);
       document.getElementsByTagName('body')[0].setAttribute('class', val);
-    } else {
+
+    }else if(val == 'ltr'){
+      this.router.navigate(
+        [], 
+        {
+          relativeTo: this.activatedRoute,
+        });
+
+      document.getElementsByTagName('html')[0].setAttribute('dir', val);
+      document.getElementsByTagName('body')[0].setAttribute('class', val);
+    }else{
       document.body?.classList.add('box-layout');
     }
   }
--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,13 +1,15 @@
-import { Component, HostListener, Inject } from "@angular/core";
+import { Component, HostListener, Inject ,OnInit} from "@angular/core";
 import { NavService } from "src/app/shared/services/nav/nav.service";
 import { LayoutService } from "src/app/shared/services/layout/layout.service";
 import { DOCUMENT } from "@angular/common";
+import { ActivatedRoute, Params, Router } from "@angular/router";
+import { log } from "../../data/component/social-app/social-app";
 @Component({
   selector: "app-header",
   templateUrl: "./header.component.html",
   styleUrls: ["./header.component.scss"],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
   public dark: boolean =
     this.layout.config.settings.layout_version == "dark-only" ? true : false;
 
@@ -15,23 +17,44 @@ export class HeaderComponent {
   constructor(
     private navServices: NavService,
     public layout: LayoutService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private activatedRoute: ActivatedRoute,
     @Inject(DOCUMENT) private document: Document
   ) {}
+
+  ngOnInit(): void {
+    this.route.queryParams
+      .subscribe((params:any) => {
+       this.document.body.classList.add(params.layout);
+      }
+    );
+  }
 
   sidebarToggle() {
     this.navServices.collapseSidebar = !this.navServices.collapseSidebar;
   }
 
   layoutToggle() {
-    this.document.body.classList.toggle("dark-only");
     this.dark = !this.dark;
+    this.router.navigate(
+      [], 
+      {
+        relativeTo: this.activatedRoute,
+        queryParams: { layout: this.dark ? 'dark-only' : 'light-only' },
+        queryParamsHandling: 'merge'
+      });
+      this.document.body.classList.toggle('dark-only')
   }
+
   languageToggle() {
     this.navServices.language = !this.navServices.language;
   }
+
   searchToggle() {
     this.navServices.search = true;
   }
+
   @HostListener("window:resize", ["$event"])
   onResize(event: any) {
     const element = document.getElementById("sidebar-toggle");
